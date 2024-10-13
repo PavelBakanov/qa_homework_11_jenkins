6dda2dec1596eea01877c3ea7bdd76abea024d32
PavelBakanov <maxconghunter@mail.ru>
2024-10-13 14:24:35 +0300

    Added attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,6 +7,9 @@ import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 public class TestBase {
     @BeforeAll
@@ -16,7 +19,13 @@ public class TestBase {
         Configuration.pageLoadStrategy = "eager";
         //Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
@@ -24,7 +33,7 @@ public class TestBase {
         Attach.screenshotAs("Последний скриншот");
         Attach.pageSource();
         Attach.browserConsoleLogs();
-
+        Attach.addVideo();
         Selenide.closeWebDriver();
     }
 }
