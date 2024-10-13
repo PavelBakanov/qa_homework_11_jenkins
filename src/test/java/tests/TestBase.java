f930b848b505a87c4a6a14d03a9b7896366201e0
PavelBakanov <maxconghunter@mail.ru>
2024-10-13 13:58:34 +0300

    Added attachments

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -3,6 +3,7 @@ package tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -16,6 +17,13 @@ public class TestBase {
         //Configuration.holdBrowserOpen = true;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         SelenideLogger.addListener("AllureSelenide",new AllureSelenide());
+    }
+
+    @AfterEach
+    protected void addAttachments()
+    {
+        Attach.screenshotAs("Последний скриншот");
+
     }
 
     @AfterEach
